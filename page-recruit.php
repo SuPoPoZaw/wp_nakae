7ca1b57d6910acbb7f1ae63212348d646b1fbb0f
Min Aung Thu Win <107828949+minAungThuWin@users.noreply.github.com>
2023-04-03 09:44:36 +0630

    feat: create table

--- page-recruit.php
+++ page-recruit.php
@@ -24,24 +24,33 @@ get_header();
                 <a class="goto" href="#recruitment-information-list">
                     <div class="goto-left-side">
                         <div class="goto-icon"><img class="inline-block" src="<?php bloginfo('template_directory'); ?>/assets/img/recruit/item-1.png" alt="募集職種のご案内" /></div>
-                        <p class="goto-description">募集職種のご案内</p>
+                        <p class="goto-description">採用情報一覧</p>
                     </div>
                 </a>
             </li>
 
             <li>
-                <a class="goto" href="#about-recruitment">
+                <a class="goto" href="#celebration-money-system">
                     <div class="goto-left-side">
                         <div class="goto-icon"><img class="inline-block" src="<?php bloginfo('template_directory'); ?>/assets/img/recruit/item-2.png" alt="職員募集について" /></div>
-                        <p class="goto-description">職員募集について</p>
+                        <p class="goto-description">就職お祝い金制度</p>
                     </div>
                 </a>
             </li>
             <li>
-                <a class="goto" href="#employment-information-list">
+                <a class="goto" href="#about-staff-recruitment">
                     <div class="goto-left-side">
                         <div class="goto-icon"><img class="inline-block" src="<?php bloginfo('template_directory'); ?>/assets/img/recruit/item-3.png" alt="採用情報" /></div>
-                        <p class="goto-description">採用情報詳細</p>
+                        <p class="goto-description">職員募集について</p>
+                    </div>
+                </a>
+            </li>
+
+            <li>
+                <a class="goto" href="#recruitment-information">
+                    <div class="goto-left-side">
+                        <div class="goto-icon"><img class="inline-block" src="<?php bloginfo('template_directory'); ?>/assets/img/recruit/item-4.png" alt="看護学生奨学金制度" /></div>
+                        <p class="goto-description">採用情報</p>
                     </div>
                 </a>
             </li>
@@ -49,103 +58,29 @@ get_header();
             <li>
                 <a class="goto" href="#nursing-scholarship">
                     <div class="goto-left-side">
-                        <div class="goto-icon"><img class="inline-block" src="<?php bloginfo('template_directory'); ?>/assets/img/recruit/item-4.png" alt="看護学生奨学金制度" /></div>
-                        <p class="goto-description">看護学生奨学金制度</p>
+                        <div class="goto-icon"><img class="inline-block" src="<?php bloginfo('template_directory'); ?>/assets/img/recruit/item-5.png" alt="看護学生奨学金制度" /></div>
+                        <p class="goto-description">看護学生 奨学金制度</p>
                     </div>
                 </a>
             </li>
-<!--
+
             <li>
-                <a class="goto" href="#certified-worker-scholarship">
+                <a class="goto" href="#care-worker-scholarship">
                     <div class="goto-left-side">
-                        <div class="goto-icon"><img class="inline-block" src="<?php bloginfo('template_directory'); ?>/assets/img/recruit/item-5.png" alt="介護福祉士奨学金制度" /></div>
-                        <p class="goto-description">介護福祉士奨学金制度</p>
+                        <div class="goto-icon"><img class="inline-block" src="<?php bloginfo('template_directory'); ?>/assets/img/recruit/item-6.png" alt="看護学生奨学金制度" /></div>
+                        <p class="goto-description">介護福祉士 奨学金制度</p>
                     </div>
                 </a>
             </li>
-        -->
+
         </ul>
     </div>
 </section>
 
 <section id="recruitment-information-list" class="item01-sec recruitment-information-list bg-gray">
     <div class="container m-w800">
-        <h2 class="h2-ttl h2-mb">募集職種のご案内</h2>
-        <p class="text-right mb20 mb10-sp">最終更新日　令和4年10月17日</p>
-        <div class="recruit-info-gp">
-            <ul class="recruit-info-li">
-                <!-- <li>
-                    <a href="#recruit-info01">医師（常勤）</a>
-                </li>
-                <li>
-                    <a href="#recruit-info02">医師（非常勤）</a>
-                </li> -->
-                <li>
-                    <a href="#recruit-info03">看護師</a>
-                </li>
-                <!-- <li>
-                    <a href="#recruit-info04">准看護師</a>
-                </li> -->
-                <!-- <li>
-                    <a href="#recruit-info05">デイケア 看護師</a>
-                </li>
-                <li>
-                    <a href="#recruit-info06">夜勤専従 看護師</a>
-                </li>
-                <li>
-                    <a href="#recruit-info07">薬剤師</a>
-                </li>
-                <li>
-                    <a href="#recruit-info08">理学療法士</a>
-                </li>
-                <li>
-                    <a href="#recruit-info09">作業療法士</a>
-                </li>
-                <li>
-                    <a href="#recruit-info10">診療放射線技師</a>
-                </li>
-                <li>
-                    <a href="#recruit-info11">臨床検査技師</a>
-                </li> -->
-                <li>
-                    <a href="#recruit-info12">看護補助者介護職員</a>
-                </li>
-                <li>
-                    <a href="#recruit-info12">介護福祉士</a>
-                </li>
-                <!--
-                <li>
-                    <a href="#recruit-info14">訪問介護員</a>
-                </li>
-                <li>
-                    <a href="#recruit-info15">介護支援専門員</a>
-                </li>
-                <li>
-                    <a href="#recruit-info16">社会福祉士</a>
-                </li>
-                <li>
-                    <a href="#recruit-info17">精神保健福祉士</a>
-                </li>
-                <li>
-                    <a href="#recruit-info18">生活相談員</a>
-                </li>
-                <li>
-                    <a href="#recruit-info19">管理栄養士</a>
-                </li>
-                <li>
-                    <a href="#recruit-info20">医事課事務</a>
-                </li>
-                <li>
-                    <a href="#recruit-info21">外来クラーク</a>
-                </li>
-                <li>
-                    <a href="#recruit-info22">調理員・調理補助</a>
-                </li>
-                <li>
-                    <a href="#recruit-info23">高卒新卒者向け</a>
-                </li> -->
-            </ul>
-        </div>
+        <h2 class="h2-ttl h2-mb">採用情報</h2>
+
     </div>
 </section>
 
@@ -153,12 +88,79 @@ get_header();
     <div class="container m-w800">
         <h2 class="h2-ttl h2-mb">職員募集について</h2>
 
+        <table class="table-dec">
+            <tbody>
+                <tr class="table-tr large-th">
+                    <th class="table-th"></th>
+                    <th class="table-th">留萌記念病院</th>
+                    <th class="table-th">野口病院</th>
+                    <th class="table-th">中江病院</th>
+                    <th class="table-th">加藤病院</th>
+                    <th class="table-th">季実の杜</th>
+                </tr>
+
+                <tr class="table-tr large-th">
+                    <th class="single-th">医師（常勤）</th>
+                    <td class="table-td"><a href="#">募集中</a></td>
+                    <td class="table-td"></td>
+                    <td class="table-td"><a href="#">募集中</a></td>
+                    <td class="table-td"></td>
+                    <td class="table-td"></td>
+                </tr>
+
+                <tr class="table-tr large-th">
+                    <th class="single-th">医師（非常勤）</th>
+                    <td class="table-td"></td>
+                    <td class="table-td"></td>
+                    <td class="table-td"></td>
+                    <td class="table-td"></td>
+                    <td class="table-td"></td>
+                </tr>
+
+                <tr class="table-tr large-th">
+                    <th class="single-th">看護師</th>
+                    <td class="table-td"><a href="#">募集中</a></td>
+                    <td class="table-td"><a href="#">募集中</a></td>
+                    <td class="table-td"><a href="#">募集中</a></td>
+                    <td class="table-td"><a href="#">募集中</a></td>
+                    <td class="table-td"><a href="#">募集中</a></td>
+                </tr>
+
+
+
+
+            </tbody>
+        </table>
+
+
+    </div>
+</section>
+
+<section id="employment-information-list" class="item03-sec employment-information-list mt70 mt50-sp bg-gray">
+    <div class="container m-w800">
+        <h2 class="h2-ttl h2-mb">採用情報詳細</h2>
+
         <div>
-            <h3 class="fz-20 fz-18-sp bd-line">募集内容</h3>
-            <p class="mb50 mb30-sp">当院では現在一緒に仕事をしてくれる、スタッフを募集しております。ご希望の方は、お電話にて各問い合わせ先までご連絡ください。熱意あるあなたをお待ちしております。</p>
+            <h3 class="fz-20 fz-18-sp bd-line fz-18-sp">募集内容</h3>
+            <p>
+                当院では現在一緒に仕事をしてくれる下記職種のスタッフを募集しております。<br />
+                正式応募前の病院（病棟）見学は随時可能です。ご希望の方はお気軽に各問い合わせ先までご連絡ください。 日程調整をさせていただきます。<br />
+                熱意あるあなたをお待ちしております。
+            </p>
+
+            <p class="text-blue pt25 mb40 mb30-sp">
+                当院ホームページをご覧いただき、直接応募いただいた方、ハローワーク経由で応募いただき、採用となった方には就職祝金制度があります。<br />
+                詳しくは、<?php $recruitment_position = scf::get('recruitment_position');
+                        if (!empty($recruitment_position)) {
+                            echo $recruitment_position;
+                        } ?>　<?php $recruitment_name = scf::get('recruitment_name');
+                                if (!empty($recruitment_name)) {
+                                    echo $recruitment_name;
+                                } ?>までお問い合わせください。
+            </p>
         </div>
 
-        <div class="recruitment-content">
+        <div class="employment-information-content">
             <div class="mb50 mb30-sp">
                 <h4 class="bg-light-indigo bd-rounded-5px mb15 pl20 fz-20 fz-18-sp leading-2">採用条件</h4>
                 <p class="pl20">
@@ -181,59 +183,9 @@ get_header();
             <div class="card-body">
                 <div class="card-body-wrapper m-auto">
                     <p>〒 <span>070-0038</span> 北海道 旭川市8条通8丁目43番地<br>医療法人社団心優会　中江病院<br><span><?php $position01 = scf::get('position01');
-                    if (!empty($position01)) { ?><?php echo $position01; ?><?php } ?></span>　<span><?php $name01 = scf::get('name01');if (!empty($name01)) { ?><?php echo $name01; ?><?php } ?></span><br>電話　<span><?php $ph_num01 = scf::get('ph_num01');if (!empty($ph_num01)) { ?><?php echo $ph_num01; ?><?php } ?></span>
-                    </p>
-                </div>
-            </div>
-        </div>
-
-    </div>
-</section>
-
-<section id="employment-information-list" class="item03-sec employment-information-list mt70 mt50-sp bg-gray">
-    <div class="container m-w800">
-        <h2 class="h2-ttl h2-mb">採用情報詳細</h2>
-
-        <div>
-            <h3 class="fz-20 fz-18-sp bd-line fz-18-sp">募集内容</h3>
-            <p>
-                当院では現在一緒に仕事をしてくれる下記職種のスタッフを募集しております。<br />
-                正式応募前の病院（病棟）見学は随時可能です。ご希望の方はお気軽に各問い合わせ先までご連絡ください。 日程調整をさせていただきます。<br />
-                熱意あるあなたをお待ちしております。
-            </p>
-
-            <p class="text-blue pt25 mb40 mb30-sp">
-                当院ホームページをご覧いただき、直接応募いただいた方、ハローワーク経由で応募いただき、採用となった方には就職祝金制度があります。<br />
-                詳しくは、<?php $recruitment_position = scf::get('recruitment_position');
-                            if (!empty($recruitment_position)) { echo $recruitment_position; } ?>　<?php $recruitment_name = scf::get('recruitment_name');
-                            if (!empty($recruitment_name)) { echo $recruitment_name; } ?>までお問い合わせください。
-            </p>
-        </div>
-
-        <div class="employment-information-content">
-            <div class="mb50 mb30-sp">
-                <h4 class="bg-light-indigo bd-rounded-5px mb15 pl20 fz-20 fz-18-sp leading-2">採用条件</h4>
-                <p class="pl20">
-                    採用条件は当社規定によります。<br />
-                    詳細は募集要項内に記載された問合せ先までお問い合わせください。
-                </p>
-            </div>
-            <div class="mb50 mb30-sp">
-                <h4 class="bg-light-indigo bd-rounded-5px mb15 pl20 fz-20 fz-18-sp leading-2">応募方法</h4>
-                <p class="pl20">下記の応募先に履歴書（写真付き）・免許証写しを郵送ください。</p>
-            </div>
-        </div>
-
-        <div class="card m-auto">
-            <div class="card-head">
-            <div class="card-head-ttl text-center">
-                    <span>共通応募先</span>
-                </div>
-            </div>
-            <div class="card-body">
-                <div class="card-body-wrapper m-auto">
-                    <p>〒 <span>070-0038</span> 北海道 旭川市8条通8丁目43番地<br>医療法人社団心優会　中江病院<br><span><?php $position01 = scf::get('position01');
-                    if (!empty($position01)) { ?> <?php echo $position01; ?><?php } ?></span>　<span><?php $name01 = scf::get('name01');if (!empty($name01)) { ?><?php echo $name01; ?><?php } ?></span><br>電話　<span><?php $ph_num01 = scf::get('ph_num01');if (!empty($ph_num01)) { ?><?php echo $ph_num01; ?><?php } ?></span>
+                                                                                            if (!empty($position01)) { ?> <?php echo $position01; ?><?php } ?></span>　<span><?php $name01 = scf::get('name01');
+                                                                                                                                                                            if (!empty($name01)) { ?><?php echo $name01; ?><?php } ?></span><br>電話　<span><?php $ph_num01 = scf::get('ph_num01');
+                                                                                                                                                                                                                                                            if (!empty($ph_num01)) { ?><?php echo $ph_num01; ?><?php } ?></span>
                     </p>
                 </div>
             </div>
@@ -277,11 +229,11 @@ get_header();
                         <tr>
                             <td>お問い合わせ</td>
                             <td>
-                                医療法人社団心優会　中江病院　<span><?php if (!empty($position02)){ ?><?php echo $position02; ?><?php } ?></span> <span><?php if (!empty($name02)){ ?><?php echo $name02; ?><?php } ?></span><br />電話　<span><?php if (!empty($ph_num02)) { ?><?php echo $ph_num02; ?><?php } ?></span><br />
-                            E-mail<br class="sp"> &lt;<?php $emai = scf::get('emai');
-                                        if (!empty($emai)) { ?>
-                            <?php echo $emai; ?>
-                            <?php } ?>&gt;
+                                医療法人社団心優会　中江病院　<span><?php if (!empty($position02)) { ?><?php echo $position02; ?><?php } ?></span> <span><?php if (!empty($name02)) { ?><?php echo $name02; ?><?php } ?></span><br />電話　<span><?php if (!empty($ph_num02)) { ?><?php echo $ph_num02; ?><?php } ?></span><br />
+                                E-mail<br class="sp"> &lt;<?php $emai = scf::get('emai');
+                                                            if (!empty($emai)) { ?>
+                                <?php echo $emai; ?>
+                                <?php } ?>&gt;
                             </td>
                         </tr>
 
@@ -332,9 +284,9 @@ get_header();
                     <tr>
                         <td>お問い合わせ</td>
                         <td>
-                            医療法人社団心優会　中江病院　<span><?php if (!empty($position02)){ ?><?php echo $position02; ?><?php } ?></span> <span><?php if (!empty($name02)){ ?><?php echo $name02; ?><?php } ?></span><br />電話　<span><?php if (!empty($ph_num02)) { ?><?php echo $ph_num02; ?><?php } ?></span><br />
+                            医療法人社団心優会　中江病院　<span><?php if (!empty($position02)) { ?><?php echo $position02; ?><?php } ?></span> <span><?php if (!empty($name02)) { ?><?php echo $name02; ?><?php } ?></span><br />電話　<span><?php if (!empty($ph_num02)) { ?><?php echo $ph_num02; ?><?php } ?></span><br />
                             E-mail<br class="sp"> &lt;<?php $emai = scf::get('emai');
-                                        if (!empty($emai)) { ?>
+                                                        if (!empty($emai)) { ?>
                             <?php echo $emai; ?>
                             <?php } ?>&gt;
                         </td>
